8ee87d404f50c78787321942ae50c8d22780df59
jordanscher <scherjo@uw.edu>
2023-03-14 16:22:27 -0700

    Add fly.io files, set up web sockets in App

--- blur-app/src/App.tsx
+++ blur-app/src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import Webcam from 'react-webcam';
-import axios from "axios";
 import Style from "./App.module.css";
+// @ts-ignore
 import styled from 'styled-components';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 const Container = styled.div`
   display: flex;
@@ -24,37 +25,29 @@ const Title = styled.h1`
 `;
 
 function App() {
-  const webcamRef = useRef(null);
+  const webcamRef = useRef<any>(null);
   const [processedImage, setProcessedImage] = useState<any>(null);
-  const socket = new WebSocket('ws://localhost:5001/blur_ws');
-  
-  const TIME_BETWEEN_FRAMES_MS = 200;
-  
-  socket.addEventListener('message', ev => {
-    // console.log(ev);
-    setProcessedImage(ev.data)
+  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://blur-app.fly.dev/blur_ws', {
+    shouldReconnect: (closeEvent) => true,
+    reconnectInterval: 1,
   });
+  
+  const TIME_BETWEEN_FRAMES_MS = 100;
+
+  useEffect(() => {
+    if (lastMessage) {
+      setProcessedImage(lastMessage.data);
+    }
+  }, [lastMessage, setProcessedImage]);
 
   const captureImage = useCallback(() => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
-      // console.log(imageSrc);
-      
-      // axios.post("https://blur-app.fly.dev/upload", {
-      // axios.post("http://localhost:5001/upload", {
-      //   image: imageSrc,
-      // })
-      //   .then((response) => {
-      //     setProcessedImage(response.data.img);
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
-      if (imageSrc != null) {
-        socket.send(imageSrc);
+      if (imageSrc && readyState === ReadyState.OPEN) {
+        sendMessage(imageSrc);
       }
     }
-  }, []); 
+  }, [readyState, sendMessage]); 
 
   useEffect(() => {
     const intervalId = setInterval(captureImage, TIME_BETWEEN_FRAMES_MS);
