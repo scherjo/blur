872bd1bd299d605c6584b7090d5cce14a4f4dc48
adrien <adrien.delepine@xemelgo.com>
2023-03-13 21:20:26 -0700

    use flask

---
+++ blur-app/src/templates/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Cam = () => {
+return (
+  <div>
+   <img
+    src="http://localhost:5001/video_feed"
+    alt="Video"
+   />
+  </div>
+ );
+};
+
+export default Cam;
